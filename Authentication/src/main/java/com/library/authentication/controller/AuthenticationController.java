5e507e5b17f436d2b6f7390c8408f59010dd4eb7
pouyehbnj <pouyehbnj@gmail.com>
2021-07-12 14:20:30 +0430

    redis session added

--- Authentication/src/main/java/com/library/authentication/controller/AuthenticationController.java
+++ Authentication/src/main/java/com/library/authentication/controller/AuthenticationController.java
@@ -1,23 +1,32 @@
-
 package com.library.authentication.controller;/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author ASUS
  */
 @Controller
+@RestController
 public class AuthenticationController {
-    	@GetMapping("/")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
-	}
+
+    @GetMapping("/")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("/test")
+    String uid(HttpSession session) {
+        return session.getId();
+    }
 }
