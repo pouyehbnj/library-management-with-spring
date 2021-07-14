7f3cd7cf481c6f696c41c559f87717aafe5e30ae
Shaghayegh Tavakoli <shaghayeghtavakoli2@gmail.com>
2021-07-14 15:37:28 +0430

    checkSession Req Body error Resolved

--- Authentication/src/main/java/com/library/authentication/controller/AuthenticationController.java
+++ Authentication/src/main/java/com/library/authentication/controller/AuthenticationController.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -67,8 +68,9 @@ public class AuthenticationController {
     private UserRepository userRepository;
 
     @RequestMapping(value = "/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = {
-        MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-    ResponseEntity login(@RequestParam Map<String, String> loginRequest, Principal principal,HttpServletResponse response) {
+            MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    ResponseEntity login(@RequestParam Map<String, String> loginRequest, Principal principal,
+            HttpServletResponse response) {
 
         String username = loginRequest.get("username");
         String password = loginRequest.get("password");
@@ -77,51 +79,57 @@ public class AuthenticationController {
         System.out.println("role:" + userRepository.findByusername(username).getRole());
         System.out.println("user:" + username);
 
-        AuthenticationManager authenticationManager = new SampleAuthenticationManager(userRepository.findByusername(username).getRole());
+        AuthenticationManager authenticationManager = new SampleAuthenticationManager(
+                userRepository.findByusername(username).getRole());
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
         Authentication authentication = (Authentication) authenticationManager.authenticate(token);
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         try {
-            // Collection<? extends Session> usersSessions = sessions.findByPrincipalName(principal.getName()).values();
+            // Collection<? extends Session> usersSessions =
+            // sessions.findByPrincipalName(principal.getName()).values();
             // for (Session session : usersSessions) {
-            //     System.out.println(session.getAttribute("SPRING_SECURITY_CONTEXT").toString());
+            // System.out.println(session.getAttribute("SPRING_SECURITY_CONTEXT").toString());
             Collection<? extends Session> usersSessions = sessions.findByPrincipalName(principal.getName()).values();
-            
-                    //
-            Cookie cookie = new Cookie("sessionID",RequestContextHolder.currentRequestAttributes().getSessionId());
+
+            //
+            Cookie cookie = new Cookie("sessionID", RequestContextHolder.currentRequestAttributes().getSessionId());
             response.addCookie(cookie);
-            
+
             for (Session session : usersSessions) {
-                System.out.println(session.getAttribute("SPRING_SECURITY_CONTEXT").toString()); 
-                
+                System.out.println(session.getAttribute("SPRING_SECURITY_CONTEXT").toString());
+
             }
         } catch (Exception exception) {
 
             return new ResponseEntity<>(authentication.getPrincipal(), HttpStatus.OK);
         }
-//        Test test = new Test();
-//        try {
-//            test.testSessionIdKeys(username);
-//        } catch (Exception ex) {
-//            System.out.println(ex);
-//            System.out.println("error ! u need to sleep now");
-//        }
+        // Test test = new Test();
+        // try {
+        // test.testSessionIdKeys(username);
+        // } catch (Exception ex) {
+        // System.out.println(ex);
+        // System.out.println("error ! u need to sleep now");
+        // }
         return new ResponseEntity<>(authentication.getPrincipal(), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/checkSessions", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = {
-         MediaType.APPLICATION_JSON_VALUE})
-    ResponseEntity checkSessions(@RequestParam Map<String, String> req) {
+            MediaType.APPLICATION_JSON_VALUE })
+    ResponseEntity checkSessions(@RequestBody Map<String, String> req) {
         String username = req.get("username");
         String id = req.get("sessionID");
-        System.out.println("user:"+username);
-        RedisHandler redis = new RedisHandler();
+        System.out.println("user --------------------- " + username);
+        System.out.println("ID --------------------- " + id);
+
         try {
+            RedisHandler redis = new RedisHandler();
             redis.findSessions(id);
         } catch (Exception ex) {
-            System.out.println("error:"+ex);
-            //Logger.getLogger(AuthenticationController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("error:" + ex);
+            return new ResponseEntity<>("Redis Error", HttpStatus.INTERNAL_SERVER_ERROR);
+            // Logger.getLogger(AuthenticationController.class.getName()).log(Level.SEVERE,
+            // null, ex);
         }
         System.out.println(RequestContextHolder.currentRequestAttributes().getSessionId());
         return ResponseEntity.ok("Authentication Confirmed");
